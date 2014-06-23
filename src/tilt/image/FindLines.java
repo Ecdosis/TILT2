c96aa91e21bc06db3174bc838b9a1c3d30f3fbe2
desmond <desmond.allan.schmidt@gmail.com>
2014-06-23 20:33:21 +1000

    Version with rudimentary findLines. Not really working yet.

--- src/tilt/image/FindLines.java
+++ src/tilt/image/FindLines.java
@@ -27,6 +27,7 @@ import java.awt.image.WritableRaster;
  */
 public class FindLines 
 {
+    float[] hAverages;
     float[] vAverages;
     float average;
     int[][] compacted;
@@ -34,51 +35,78 @@ public class FindLines
     int[][] peaks;
     int height;
     int width;
-    static float SCALE_RATIO = 0.005f;
+    static float SCALE_RATIO = 0.006f;
     /** number of vertical or horizontal pixels to combine */
     int scale;
     public FindLines( WritableRaster wr )
     {
-        scale = Math.round(wr.getWidth()/SCALE_RATIO);
-        height = wr.getHeight()/scale;
-        width = wr.getWidth()/scale;
-        compacted = new int[height][width];
+        scale = Math.round(wr.getWidth()*SCALE_RATIO);
+        height = (wr.getHeight()+scale-1)/scale;
+        width = (wr.getWidth()+scale-1)/scale;
+        compacted = new int[width][height];
+        hAverages = new float[height];
         vAverages = new float[width];
         peaks = new int[width][];
-        cusum = new float[height][width];
+        cusum = new float[width][height];
+        compact( wr );
+        // average the rows
+        average = 0.0f;
+        for ( int y=0;y<height;y++ )
+        {
+            hAverages[y] = averageRow(y);
+            average += hAverages[y];
+        }
+        // calculate the cusums for each row
+        average /= height;
+        for ( int y=0;y<height;y++ )
+        {
+            cusum[0][y] = 0.0f;
+            for ( int x=1;x<width;x++ )
+            {
+                cusum[x][y] = cusum[x-1][y]+(compacted[x][y]-hAverages[y]);
+            }
+        }
         // average the columns
         for ( int x=0;x<width;x++ )
         {
             vAverages[x] = averageCol(x);
-            average += vAverages[x];
         }
-        // calculate the cusum
-        average /= width;
+        // calculate the cusums for each column
         for ( int x=0;x<width;x++ )
         {
-            cusum[0][x] = 0.0f;
+            cusum[x][0] = 0.0f;
             for ( int y=1;y<height;y++ )
             {
-                cusum[y][x] = cusum[y-1][x]+(cusum[y][x]-vAverages[x]);
+                cusum[x][y] = cusum[x][y-1]+(cusum[x][y]-vAverages[x]);
             }
         }
         // find peaks
         ArrayList<Integer> peaks = new ArrayList<Integer>();
         int totalPeaks = 0;
-        for ( int x=0;x<width;x++ )
+        for ( int y=0;y<height;y++ )
         {
             float current = 0.0f;
-            for ( int y=0;y<height;y++ )
+            int index = -1;
+            for ( int x=0;x<width;x++ )
             {
-                if ( cusum[y][x] > average )
+                if ( cusum[x][y] > average )
                 {
-                    if ( cusum[y][x] > current )
-                        current = cusum[y][x];
-                    else if ( current != 0.0f && cusum[y][x] < current )
+                    if ( cusum[x][y] > current )
                     {
-                        peaks.add(new Integer(y) );
-                        current = 0.0f;
+                        current = cusum[x][y];
+                        index = y;
                     }
+                    else if ( cusum[x][y] != current )
+                    {
+                        peaks.add(new Integer(index) );
+                        current = 0.0f;
+                        index = -1;
+                    }
+                }
+                else if ( current > 0.0f )
+                {
+                    peaks.add(new Integer(index) );
+                    current = 0.0f;index = -1;
                 }
             }
             totalPeaks += peaks.size();
@@ -90,43 +118,47 @@ public class FindLines
         // closest matching values, allowing some to peter out 
         // or new ones to start
     }
+    /**
+     * Collapse the whole image by merging squares of pixels
+     * @param wr the raster with the original pixels
+     */
     void compact( WritableRaster wr )
     {
+        int[] iArray = new int[1];
+        int h = wr.getHeight();
+        int w = wr.getWidth();
+        for ( int y=0;y<h;y++ )
+        {
+            for ( int x=0;x<w;x++ )
+            {
+                wr.getPixel( x, y, iArray );
+                if ( iArray[0] == 0 )
+                    compacted[x/scale][y/scale]++;
+            }
+        }
     }
     /**
      * Compute the average of a row
-     * @param wr the raster with the pixels
      * @param row the row number
      * @return the average pixel intensity for the row
      */
-    float averageRow( WritableRaster wr, int row )
+    float averageRow( int row )
     {
-        int total = 0;
-        int[] iArray = new int[1];
-        int rowWidth = wr.getWidth()/SCALE;
-        for ( int i=0;i<rowWidth;i++ )
-        {
-            for ( int y=0;y<SCALE;y++ )
-            {
-                for ( int x=0;x<SCALE;x++ )
-                {
-                    wr.getPixel(i*SCALE+x,row*SCALE+y,iArray );
-                    total += (iArray[0]==0)?1:0;
-                }
-            }
-        }
-        return (float)total/(float)rowWidth;
+        float total = 0.0f;
+        for ( int i=0;i<width;i++ )
+            total += compacted[i][row];
+        return total/(float)width;
     }
     /**
-     * Compute the average of a row
-     * @param col the column number
-     * @return the average pixel intensity for the column
+     * Compute the average of a column
+     * @param row the col number
+     * @return the average cusum value for the col
      */
     float averageCol( int col )
     {
         float total = 0.0f;
         for ( int i=0;i<height;i++ )
-            total += cusum[i][col];
+            total += cusum[col][i];
         return total/(float)height;
     }
 }
