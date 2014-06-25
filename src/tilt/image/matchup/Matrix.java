8f1cc33ec5f9833aef127c40c6a456d6fb3aecb7
desmond <desmond.allan.schmidt@gmail.com>
2014-06-25 11:07:23 +1000

    Deleted IntDiff class.

--- src/tilt/image/matchup/Matrix.java
+++ src/tilt/image/matchup/Matrix.java
@@ -210,4 +210,35 @@ public class Matrix
         }
         return path;
     }
+    /**
+     * For testing
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        if ( args.length==2)
+        {
+            try
+            {
+                String[] arg1 = args[0].split(",");
+                String[] arg2 = args[1].split(",");
+                int[] list1 = new int[arg1.length];
+                int[] list2 = new int[arg2.length];
+                for ( int i=0;i<arg1.length;i++ )
+                    list1[i] = Integer.parseInt(arg1[i]);
+                for ( int i=0;i<arg2.length;i++ )
+                    list2[i] = Integer.parseInt(arg2[i]);
+                Matrix m = new Matrix( list1, list2 );
+                System.out.println(m.toString());
+                m.traceBack();
+            }
+            catch ( Exception e )
+            {
+                e.printStackTrace(System.out);
+            }
+        }
+        else
+            System.out.println("usage: java IntDiff <list1> <list2>");
+        
+    }
 }
