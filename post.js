94d9e74d9837bcf10efaf413d29e04a851eb5176
desmond <desmond.allan.schmidt@gmail.com>
2014-07-27 19:30:31 +1000

    Finished draft of post.js with mouse-movement over polygon testing.

--- post.js
+++ post.js
@@ -66,6 +66,10 @@ function Point( x, y )
 {
     this.x = x;
     this.y = y;
+    this.minus = function(p)
+    {
+        return new Point(this.x-p.x,this.y-p.y);
+    }
 }
 function scalePoints( coords )
 {
@@ -88,6 +92,32 @@ function Bounds( x, y, width, height )
     this.y = y;
     this.width = width;
     this.height = height;
+    this.within = function( r )
+    {
+        return this.x>=r.x 
+            && this.y>=r.y 
+            && this.x+this.width<r.x+r.width 
+            && this.y+this.height<r.y+r.height;
+    }
+    this.outside = function( r )
+    {
+        this.bottom()<r.y
+        || this.y>=r.bottom()
+        || this.right()<r.x
+        || this.x >=r.right()
+    }
+    this.right = function()
+    {
+        return this.x+this.width;
+    }
+    this.bottom = function()
+    {
+        return this.y+this.height;
+    }
+    this.hasPt( x, y )
+    {
+        return x>=this.x&&y>=this.y&&y<this.bottom&&x<this.right();
+    }
 }
 function Polygon( coords, props )
 {
@@ -99,6 +129,7 @@ function Polygon( coords, props )
             this.hyphen = hyphen;
     }
     this.points = scalePoints(coords);
+    this.SMALL_NUM = 0.00000001;
     var str = "";
     var x=Number.MAX_VALUE,y=Number.MAX_VALUE;
     var bot=0,right=0;
@@ -112,15 +143,236 @@ function Polygon( coords, props )
             right = points[i].x;
         if ( points[i].y > bot )
             bot = points[i].y
-        str += points[i].x+" "+points[i].y+" ";
     }
     this.bounds = new Bounds( x, y, right-x, bot-y );
-    alert(str);
+    this.dot = function(u,v)
+    {
+        return ((u).x * (v).x + (u).y * (v).y)
+    }
+    this.perp = function(u,v)  
+    {
+        return ((u).x * (v).y - (u).y * (v).x);
+    }
+    this.cn_PnPoly = function( P, V, n )
+    {
+        var cn = 0;
+        for (var i=0; i<n; i++) 
+        {
+            if (((V[i].y <= P.y) && (V[i+1].y > P.y))
+            || ((V[i].y > P.y) && (V[i+1].y <=  P.y))) 
+            {
+                var vt = (P.y  - V[i].y) / (V[i+1].y - V[i].y);
+                if (P.x <  V[i].x + vt * (V[i+1].x - V[i].x))
+                     ++cn;
+            }
+        }
+        return cn&1;
+    }
+    this.intersectsLine = function( p0, p1 )
+    {
+        if ( p0 == p1 )      
+            return cn_PnPoly( p0, V, n );
+        var tE = 0;
+        var tL = 1;
+        var t, N, D;
+        var dS = p1.minus(p0);
+        var e; 
+        for ( var i=0; i<this.points.length; i++)
+        {
+            e = this.points[i+1].minus(this.points[i]);
+            N = perp(e, p0.minus(this.points[i]));
+            D = -perp(e, dS);
+            if (Math.abs(D) < SMALL_NUM) 
+            {
+                if (N < 0)
+                     return false;
+                else
+                     continue;
+            }
+            t = N / D;
+            if (D < 0) 
+            {
+                if (t > tE) 
+                {
+                     tE = t;
+                     if (tE > tL)
+                         return false;
+                }
+            }
+            else 
+            {
+                if (t < tL) 
+                {
+                     tL = t;
+                     if (tL < tE)
+                         return false;
+                }
+            }
+        }
+        return true;
+    }
+    this.overlaps = function( bounds )
+    {
+        if (this.bounds.within(bounds) )
+           return true;
+        else if ( this.bounds.outside(bounds) )
+            return false;
+        else 
+        {
+            var p0 = new Point(bounds.x,bounds.y);
+            var p1 = new Point(bounds.right(),bounds.y);
+            var p2 = new Point(bounds.x,bounds.bottom());
+            var p3 = new Point(bounds.right().bounds.bottom());
+            return this.intersectsLine(p0,p1)
+                || this.intersectsLine(p1,p3)
+                || this.intersectsLine(p2,p3)
+                || this.intersectsLine(p0,p2);
+        }
+    }
+}
+function Quadrant( bounds )
+{
+    this.bounds = bounds;
+    this.add = function( poly )
+    {
+        if ( poly.overlaps(this.bounds) )
+        {
+            if ( this.children == undefined && this.poly == undefined )
+                this.poly = poly;
+            else
+            {
+                setTopLeft(poly);
+                if ( this.poly != undefined )
+                    setTopLeft(this.poly);
+                setTopRight(poly);
+                if ( this.poly != undefined )
+                    setTopRight(this.poly);
+                setBotLeft(poly);
+                if ( this.poly != undefined )
+                    setBotLeft(this.poly);
+                setBotRight(poly);
+                if ( this.poly != undefined )
+                    setBotRight(this.poly);
+                this.poly = undefined;
+                this.children = true;
+            }
+        }
+    }
+    this.setTopLeft = function( poly )
+    {
+        if ( this.tl == undefined )
+        {
+            var b = new Bounds(
+                this.bound.x,
+                this.bounds.y,
+                Math.round(this.bounds.width/2),
+                Math.round(this.bounds.height/2)
+            );
+            if ( poly.overlaps(b) )
+            {
+                this.tl = new Quadrant(b);
+                this.tl.poly = poly;
+            }
+        }
+        else
+            this.tl.add( poly );
+    }
+    this.setTopRight = function( poly )
+    {
+        if ( this.tl == undefined )
+        {
+            var newX = this.bounds.x+this.bounds.width/2;
+            var b = new Bounds(
+                newX, 
+                this.bounds.y,
+                Math.round(this.bounds.right()-newX),
+                Math.round(this.bounds.height/2)
+            );
+            if ( poly.overlaps(b) )
+            {
+                this.tr = new Quadrant(b);
+                this.tr.poly = poly;
+            }
+        }
+        else
+            this.tr.add( poly );
+    }
+    this.setBotLeft = function( poly )
+    {
+        if ( this.bl == undefined )
+        {
+            var newY = this.bounds.y+this.bounds.height/2;
+            var b = new Bounds(
+                this.bound.x,
+                newY,
+                Math.round(this.bounds.width/2),
+                Math.round(this.bounds.bottom()-newY)
+            );
+            if ( poly.overlaps(b) )
+            {
+                this.bl = new Quadrant(b);
+                this.bl.poly = poly;
+            }
+        }
+        else
+            this.bl.add( poly );
+    }
+    this.setBotRight = function( poly )
+    {
+        if ( this.br == undefined )
+        {
+            var newX = this.bounds.x+this.bounds.width/2;
+            var newY = this.bounds.y+this.bounds.height/2;
+            var b = new Bounds(
+                newX,
+                newY,
+                Math.round(this.bounds.right()-newX),
+                Math.round(this.bounds.bottom()-newY)
+            );
+            if ( poly.overlaps(b) )
+            {
+                this.br = new Quadrant(b);
+                this.br.poly = poly;
+            }
+        }
+        else
+            this.br.add( poly );
+    }
+    this.find = function( x, y )
+    {
+        if ( this.bounds.hasPt(x,y) )
+        { 
+            if ( !this.children && this.poly != undefined )
+                return this.poly;
+            else
+            {
+                var poly = undefined;
+                if ( this.tl != undefined )
+                    poly = this.tl.find(x,y);
+                if ( poly==undefined && this.tr != undefined )
+                    poly = this.tr.find(x,y);
+                if ( poly==undefined && this.bl != undefined )
+                    poly = this.bl.find(x,y);
+                if ( poly==undefined && this.br != undefined )
+                    poly = this.br.find(x,y);
+                return poly;
+            }
+        }
+        else
+            return undefined;
+    }
 }
 function QuadTree( json )
 {
     var geoJson = JSON.parse( json );
-    //this.root = QTNode(geoJson.bounds);
+    var canvas = $("#left canvas");
+    var ht = canvas.height();
+    var wt = canvas.width();
+    var x1 = Math.round(geoJson.bounds[0]*wt);
+    var y1 = Math.round(geojson.bounds[1]*ht);
+    var x2 = Math.round(geoJson.bounds[2]*wt);
+    var y2 = Math.round(geojson.bounds[3]*ht);
+    this.root = Quadrant( new Bounds(x1,y1,x2-x1,y2-y1) );
     for ( var i=0;i<geoJson.features.length;i++ )
     {
         var line = geoJson.features[i];
@@ -134,10 +386,14 @@ function QuadTree( json )
                 {
                     var p = Polygon(geometry.coordinates,
                         poly.properties);
-                    //qtStore( p );
+                    this.root.add( p );
                 }
             }
         }
+    }
+    this.find = function( x, y )
+    {
+        return this.root.find( x, y );
     }
 }
 function bindJsonToImage( json )
