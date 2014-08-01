bdef44b888141d16f1e081eed9eefc98b5815083
desmond <desmond.allan.schmidt@gmail.com>
2014-08-02 07:11:33 +1000

    Update some headers.

--- src/tilt/image/convexhull/Point2D.java
+++ src/tilt/image/convexhull/Point2D.java
@@ -1,8 +1,27 @@
+/*
+ * This file is part of TILT.
+ *
+ *  TILT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TILT is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TILT.  If not, see <http://www.gnu.org/licenses/>.
+ *  (c) copyright Desmond Schmidt 2014
+ */
+
 /*************************************************************************
 * Compilation: javac Point2D.java
 *
-* Immutable point data type for points in the plane.
-* from https://github.com/aistrate/AlgorithmsSedgewick/blob/master/1-Fundamentals/1-2-DataAbstraction/Point2D.java
+* Mutable point data type for points in the plane.
+* from https://github.com/aistrate/AlgorithmsSedgewick/blob/master/
+* 1-Fundamentals/1-2-DataAbstraction/Point2D.java
 *************************************************************************/
 package tilt.image.convexhull;
 
@@ -17,6 +36,7 @@ public class Point2D implements Comparable<Point2D> {
     public final Comparator<Point2D> ATAN2_ORDER = new Atan2Order();
     public final Comparator<Point2D> DISTANCE_TO_ORDER = new DistanceToOrder();
 
+    // made these mutable
     public double x; // x coordinate
     public double y; // y coordinate
 
